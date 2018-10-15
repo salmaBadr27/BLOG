1a8520dd9f13db84c852bdfebe8cbd84629ba32e
salma badr <beauty_saloma@outlook.com>
2018-10-15 16:11:25 +0200

    fix

--- config/database.php
+++ config/database.php
@@ -1,6 +1,11 @@
 <?php
-
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"],1);
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -13,7 +18,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,6 +72,18 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+        
+        'pgsql_production' => [
+            'driver'   => 'pgsql',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
