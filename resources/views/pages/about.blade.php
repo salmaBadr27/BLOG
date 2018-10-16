616c0b1517fe2e2aca683c378e1031554e00cf0a
salma badr <beauty_saloma@outlook.com>
2018-10-16 09:27:12 +0200

    fix

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,8 +1,7 @@
 @extends('layouts.app')
     @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome To Laravel!</h1>
-        <p>This is the laravel application from the "Laravel From Scratch" YouTube series</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    <h1><?php echo $title?></h1>
+    <h3>you are in about page , welcome bek ya 7ag</h3>
     </div>
-        @endsection
+    @endsection
