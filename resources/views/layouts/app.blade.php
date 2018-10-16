634f140efacf2234ae72044055873e4c5c9cf277
salma badr <beauty_saloma@outlook.com>
2018-10-16 10:37:42 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,9 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="/../vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-    {{-- <script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
         CKEDITOR.replace( 'article-ckeditor' );
-    </script> --}}
+    </script>
 </body>
 </html>
