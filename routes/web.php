2e8823d19cbcd71be4c9d9f33fda7f07b70650be
salma badr <beauty_saloma@outlook.com>
2018-10-16 09:17:24 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::get('/dashboard', 'DashboardController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
