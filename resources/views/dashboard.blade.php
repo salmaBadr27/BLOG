400aaf3a649dcc08553a6a9150d31702033a56ec
salma badr <beauty_saloma@outlook.com>
2018-10-16 11:05:17 +0200

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -7,36 +6,31 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    <a href="/posts/create" class="btn btn-primary">Create Post</a>
+    <h1>Posts</h1>
+    <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <h3>Your Blog Posts</h3>
-                    @if(count([$posts]) > 0)
-                        <table class="table table-striped">
-                            <tr>
-                                <th>Title</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                            @foreach($posts as $post)
-                                <tr>
-                                    <td>{{$post->title}}</td>
-                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
-                                    <td>
-                                        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                            {{Form::hidden('_method', 'DELETE')}}
-                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                                        {!!Form::close()!!}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    @else
-                        <p>You have no posts</p>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    @if(count($posts) > 0)
+    <table class="table table-striped">
+        <tr>
+            <th>Title</th>
+            <th></th>
+            <th></th>
+        </tr>
+        @foreach($posts as $post)
+        <tr>
+            <td>{{$post->title}}</td>
+            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+            <td>
+                {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                    {{Form::hidden('_method', 'DELETE')}}
+                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                {!!Form::close()!!}
+            </td>
+        </tr>
+        @endforeach
+       
+    @else
+        <p>No posts found</p>
+    @endif
 @endsection
